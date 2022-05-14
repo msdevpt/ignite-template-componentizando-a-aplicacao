436f622ede16abe63e3d027b6de2475fffd54770
M.Silva <msilva.kb@outlook.com>
2022-05-14 21:04:07 +0100

    feat: concluir desafio 2

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from './Button';
+
+interface SideBarProps{
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar( {genres, selectedGenreId, handleClickButton} : SideBarProps) {
+  return(
+    <>
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+          />
+          ))}
+      </div>
+    </>
+  )
 }
